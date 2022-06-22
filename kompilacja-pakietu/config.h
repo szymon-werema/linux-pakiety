4a9af7b0c819bbc9b1f45e627557714892a52828
kali <kali@kali.home>
2022-06-22 23:12:48 +0200

    kompilacja pakietu

---
+++ kompilacja-pakietu/config.h
@@ -0,0 +1,129 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 when help documentation is built */
+#define ENABLE_HELP_DOC 1
+
+/* Define to 1 to enable deletion of old partitions before creating a loop
+   table workaround */
+/* #undef ENABLE_LOOP_DELETE_OLD_PTNS_WORKAROUND */
+
+/* always defined to indicate that i18n is enabled */
+#define ENABLE_NLS 1
+
+/* Define to 1 if online resize is enabled */
+#define ENABLE_ONLINE_RESIZE 1
+
+/* description */
+#define GETTEXT_PACKAGE "gparted"
+
+/* Define to 1 if you have the `bind_textdomain_codeset' function. */
+#define HAVE_BIND_TEXTDOMAIN_CODESET 1
+
+/* Define to 1 if you have the Mac OS X function CFLocaleCopyCurrent in the
+   CoreFoundation framework. */
+/* #undef HAVE_CFLOCALECOPYCURRENT */
+
+/* Define to 1 if you have the Mac OS X function CFPreferencesCopyAppValue in
+   the CoreFoundation framework. */
+/* #undef HAVE_CFPREFERENCESCOPYAPPVALUE */
+
+/* define if the compiler supports basic C++11 syntax */
+#define HAVE_CXX11 1
+
+/* Define to 1 if you have the `dcgettext' function. */
+#define HAVE_DCGETTEXT 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define if the GNU gettext() function is already present or preinstalled. */
+#define HAVE_GETTEXT 1
+
+/* Define to 1 if gtk provides gtk_show_uri_on_window function. */
+#define HAVE_GTK_SHOW_URI_ON_WINDOW 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if gtkmm provides Gtk::Label::set_xalign() method. */
+#define HAVE_LABEL_SET_XALIGN 1
+
+/* Define if your <locale.h> file defines LC_MESSAGES. */
+#define HAVE_LC_MESSAGES 1
+
+/* Define to 1 if you have the `dl' library (-ldl). */
+#define HAVE_LIBDL 1
+
+/* Define to 1 if you have the `parted' library (-lparted). */
+#define HAVE_LIBPARTED 1
+
+/* Define to 1 if have libparted fs resize capability */
+#define HAVE_LIBPARTED_FS_RESIZE 1
+
+/* Define to 1 if you have the `uuid' library (-luuid). */
+#define HAVE_LIBUUID 1
+
+/* Define to 1 if you have the <locale.h> header file. */
+#define HAVE_LOCALE_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if gtkmm provides
+   Gtk::ScrolledWindow::set_propagate_natural_width() method. */
+#define HAVE_SET_PROPAGATE_NATURAL_WIDTH 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+#define PACKAGE "gparted"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "https://gitlab.gnome.org/GNOME/gparted/issues"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "gparted"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "gparted 1.4.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "gparted"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.4.0"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to 1 to use native libparted /dev/mapper dmraid support */
+/* #undef USE_LIBPARTED_DMRAID */
+
+/* Version number of package */
+#define VERSION "1.4.0"
---
+++ kompilacja-pakietu/src/nilfs2.cc
@@ -0,0 +1,219 @@
+/* Copyright (C) 2011 Mike Fleetwood
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "nilfs2.h"
+#include "FileSystem.h"
+#include "Partition.h"
+
+#include <glibmm/miscutils.h>
+#include <glibmm/shell.h>
+
+
+namespace GParted
+{
+
+FS nilfs2::get_filesystem_support()
+{
+	FS fs( FS_NILFS2 );
+
+	fs .busy = FS::GPARTED ;
+
+	if ( ! Glib::find_program_in_path( "mkfs.nilfs2" ) .empty() )
+	{
+		fs.create = FS::EXTERNAL;
+		fs.create_with_label = FS::EXTERNAL;
+	}
+
+	if ( ! Glib::find_program_in_path( "nilfs-tune" ) .empty() )
+	{
+		fs.read = FS::EXTERNAL;
+		fs.read_label = FS::EXTERNAL;
+		fs.write_label = FS::EXTERNAL;
+		fs.read_uuid = FS::EXTERNAL;
+		fs.write_uuid = FS::EXTERNAL;
+	}
+
+	//Nilfs2 resizing is an online only operation and needs:
+	//  mount, umount, nilfs-resize and linux >= 3.0 with nilfs2 support.
+	if ( ! Glib::find_program_in_path( "mount" ) .empty()        &&
+	     ! Glib::find_program_in_path( "umount" ) .empty()       &&
+	     ! Glib::find_program_in_path( "nilfs-resize" ) .empty() &&
+	     Utils::kernel_supports_fs( "nilfs2" )                   &&
+	     Utils::kernel_version_at_least( 3, 0, 0 )                  )
+	{
+		fs.grow = FS::EXTERNAL;
+		if ( fs .read ) //needed to determine a minimum file system size.
+			fs.shrink = FS::EXTERNAL;
+	}
+
+	fs.copy = FS::GPARTED;
+	fs.move = FS::GPARTED;
+	fs .online_read = FS::GPARTED ;
+#ifdef ENABLE_ONLINE_RESIZE
+	if ( Utils::kernel_version_at_least( 3, 6, 0 ) )
+	{
+		fs .online_grow = fs .grow ;
+		fs .online_shrink = fs .shrink ;
+	}
+#endif
+
+	//Minimum FS size is 128M+4K using mkfs.nilfs2 defaults
+	fs_limits.min_size = 128 * MEBIBYTE + 4 * KIBIBYTE;
+
+	return fs ;
+}
+
+void nilfs2::set_used_sectors( Partition & partition )
+{
+	if ( ! Utils::execute_command( "nilfs-tune -l " + Glib::shell_quote( partition.get_path() ),
+	                               output, error, true )                                         )
+	{
+		//File system size in bytes
+		Glib::ustring::size_type index = output .find( "Device size:" ) ;
+		if (   index == Glib::ustring::npos
+		    || sscanf( output.substr( index ).c_str(), "Device size: %lld", &T ) != 1
+		   )
+			T = -1 ;
+
+		//Free space in blocks
+		index = output .find( "Free blocks count:" ) ;
+		if (   index == Glib::ustring::npos
+		    || sscanf( output.substr( index ).c_str(), "Free blocks count: %lld", &N ) != 1
+		   )
+			N = -1 ;
+
+		index = output .find( "Block size:" ) ;
+		if (   index == Glib::ustring::npos
+		    || sscanf( output.substr( index ).c_str(), "Block size: %lld", &S ) != 1
+		   )
+			S = -1 ;
+
+		if ( T > -1 && N > -1 && S > -1 )
+		{
+			T = Utils::round( T / double(partition .sector_size) ) ;
+			N = Utils::round( N * ( S / double(partition .sector_size) ) ) ;
+			partition .set_sector_usage( T, N ) ;
+			partition.fs_block_size = S;
+		}
+	}
+	else
+	{
+		if ( ! output .empty() )
+			partition.push_back_message( output );
+
+		if ( ! error .empty() )
+			partition.push_back_message( error );
+	}
+}
+
+void nilfs2::read_label( Partition & partition )
+{
+	if ( ! Utils::execute_command( "nilfs-tune -l " + Glib::shell_quote( partition.get_path() ),
+	                               output, error, true )                                         )
+	{
+		Glib::ustring label = Utils::regexp_label( output, "^Filesystem volume name:[\t ]*(.*)$" ) ;
+		if ( label != "(none)" )
+			partition.set_filesystem_label( label );
+		else
+			partition.set_filesystem_label( "" );
+	}
+	else
+	{
+		if ( ! output .empty() )
+			partition.push_back_message( output );
+
+		if ( ! error .empty() )
+			partition.push_back_message( error );
+	}
+}
+
+bool nilfs2::write_label( const Partition & partition, OperationDetail & operationdetail )
+{
+	return ! execute_command( "nilfs-tune -L " + Glib::shell_quote( partition.get_filesystem_label() ) +
+	                          " " + Glib::shell_quote( partition.get_path() ),
+	                          operationdetail, EXEC_CHECK_STATUS );
+}
+
+void nilfs2::read_uuid( Partition & partition )
+{
+	if ( ! Utils::execute_command( "nilfs-tune -l " + Glib::shell_quote( partition.get_path() ),
+	                               output, error, true )                                         )
+	{
+		partition .uuid = Utils::regexp_label( output, "^Filesystem UUID:[[:blank:]]*(" RFC4122_NONE_NIL_UUID_REGEXP ")" ) ;
+	}
+	else
+	{
+		if ( ! output .empty() )
+			partition.push_back_message( output );
+
+		if ( ! error .empty() )
+			partition.push_back_message( error );
+	}
+}
+
+bool nilfs2::write_uuid( const Partition & partition, OperationDetail & operationdetail )
+{
+	return ! execute_command( "nilfs-tune -U " + Glib::shell_quote( Utils::generate_uuid() ) +
+	                          " " + Glib::shell_quote( partition .get_path() ),
+	                          operationdetail, EXEC_CHECK_STATUS );
+}
+
+bool nilfs2::create( const Partition & new_partition, OperationDetail & operationdetail )
+{
+	return ! execute_command( "mkfs.nilfs2 -L " + Glib::shell_quote( new_partition.get_filesystem_label() ) +
+	                          " " + Glib::shell_quote( new_partition.get_path() ),
+	                          operationdetail, EXEC_CHECK_STATUS );
+}
+
+bool nilfs2::resize( const Partition & partition_new, OperationDetail & operationdetail, bool fill_partition )
+{
+	bool success = true ;
+
+	Glib::ustring mount_point ;
+	if ( ! partition_new .busy )
+	{
+		mount_point = mk_temp_dir( "", operationdetail ) ;
+		if ( mount_point .empty() )
+			return false ;
+
+		success &= ! execute_command( "mount -v -t nilfs2 " + Glib::shell_quote( partition_new.get_path() ) +
+		                              " " + Glib::shell_quote( mount_point ),
+		                              operationdetail, EXEC_CHECK_STATUS );
+	}
+
+	if ( success )
+	{
+		Glib::ustring cmd = "nilfs-resize -v -y " + Glib::shell_quote( partition_new.get_path() );
+		if ( ! fill_partition )
+		{
+			Glib::ustring size = Utils::num_to_str( floor( Utils::sector_to_unit(
+					partition_new .get_sector_length(), partition_new .sector_size, UNIT_KIB ) ) ) + "K" ;
+			cmd += " " + size ;
+		}
+		success &= ! execute_command( cmd, operationdetail, EXEC_CHECK_STATUS );
+
+		if ( ! partition_new. busy )
+			success &= ! execute_command( "umount -v " + Glib::shell_quote( mount_point ),
+			                              operationdetail, EXEC_CHECK_STATUS );
+	}
+
+	if ( ! partition_new .busy )
+		rm_temp_dir( mount_point, operationdetail ) ;
+
+	return success ;
+}
+
+} //GParted
